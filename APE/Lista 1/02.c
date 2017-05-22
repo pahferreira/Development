0f17d1296f35cda029880d0edbcfdc2adf5aa199
pahferreira <ph.equfpb@gmail.com>
2017-05-21 23:37:03 -0300

    Exercício 02

---
+++ APE/Lista 1/02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int x, y;
+
+    printf("Digite um numero inteiro, para obter o triplo do mesmo! \n");
+    scanf("%d",&x);
+    y = x * 3;
+    printf("O triplo do seu numero e %2d", y);
+
+
+    return 0;
+}

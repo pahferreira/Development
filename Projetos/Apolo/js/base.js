1f79d8a1b3dd1c53eb7339132cb6d653ffca2d08
Willian Abdon <willian.abdom@hotmail.com>
2017-07-30 18:29:54 -0300

    Correção do Footer e melhorias na classe integrantes

--- Projetos/Apolo/js/base.js
+++ Projetos/Apolo/js/base.js
@@ -5,6 +5,11 @@ $(function() {
           } else {
             $(".menu").css("background", "rgba(0, 0, 0, 0.57)");
           }
+          if($(window).scrollTop() > 1900) {
+            $(".menu").css("border-bottom", "2px solid white");
+          } else {
+            $(".menu").css("border-bottom", "0");
+          }
     });
 });
 jQuery(document).ready(function($) {
